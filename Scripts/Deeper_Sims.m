ec219536cbdb1239aa49e2301d45607d7d8ffb43
Joe Feldman <joefeldman396@gmail.com>
2026-04-17 21:15:54 -0500

    hyperparameter tuning for deeper sims

--- Scripts/Deeper_Sims.m
+++ Scripts/Deeper_Sims.m
@@ -32,33 +32,31 @@ addpath('/Users/jfeldm01/Library/CloudStorage/OneDrive-Kearney/Documents/Deep-Di
 % -------------------------------------------------------------
 D = 3;
 
-K3_true = 3;
-K2_true = 10;
-K1_true = 30;
+K3_true = 1;
+K2_true = 3;
+K1_true = 10;
 
-J = 150;
+J = 120;
 
-%% ------------------------------------------------------------
-% Construct true loading matrices
-% -------------------------------------------------------------
-B3_sub = zeros(K2_true, K3_true);
+B3_sub = 3*ones(3,1);
+B2_sub = zeros(K1_true, K2_true);
 
 max_val = 4;
-idx = floor(linspace(1, K2_true, 4));
+idx = floor(linspace(1, K1_true, 4));
 for i = 1:(length(idx) - 1)
     if mod(i,2) == 0
-        B3_sub(idx(i):idx(i+1), i)   = max_val;
-        B3_sub(idx(i):idx(i+1), i-1) = -max_val/3;
+        B2_sub(idx(i):idx(i+1), i)   = max_val;
+        B2_sub(idx(i):idx(i+1), i-1) = -max_val/3;
     else
-        B3_sub(idx(i):idx(i+1), i) = max_val;
+        B2_sub(idx(i):idx(i+1), i) = max_val;
         if i < length(idx) - 2
-            B3_sub(idx(i):idx(i+1), i+1) = -max_val/3;
+            B2_sub(idx(i):idx(i+1), i+1) = -max_val/3;
         end
     end
 end
 
-B2_sub = sim_block_loadings_test(K1_true, K2_true,K2_true, 3, 4, 4/3, 0,1);
-B1_sub = sim_block_loadings_test(J, K1_true,K1_true,5,10,5,0,1);
+B1_sub = sim_block_loadings(J, K1_true,12,10,5,0,1); % first entry by J: J = 50 -> 5, J = 100 -> 10, J = 150 -> 15
+
 
 B3_true = [-2 * ones(K2_true,1), B3_sub];
 B2_true = [-2 * ones(K1_true,1), B2_sub];
@@ -94,9 +92,9 @@ n_vec      = [4000, 8000,12000,16000];
 
 % Initial layer sizes for fitting
 K_cell_init = cell(1,D);
-K_cell_init{1} = 50;
-K_cell_init{2} = 16;
-K_cell_init{3} = 5;
+K_cell_init{1} = 40;
+K_cell_init{2} = 13;
+K_cell_init{3} = 4;
 
 epsilon_init = 1e-4;
 
@@ -105,8 +103,8 @@ epsilon_init = 1e-4;
 C        = 1;                 % MC draws inside SAEM step
 it       = 50;                % max iterations
 temp     = .7;               % initial temperature
-tau      = .3;               % threshold level
-t_spike  = [0.01, 0.02, 0.01]; % layer-specific spike scales
+            % threshold level
+t_spike  = [0.02, 0.04, 0.01]; % layer-specific spike scales
 
 
 %% ------------------------------------------------------------
@@ -168,7 +166,7 @@ for aa = 1:numel(n_vec)
         %% ----------------------------------------------------
         % 5) Initialize latent Gaussian Z
         % -----------------------------------------------------
-        % Option A: mixture-based init
+        % mixture-based init
         Z_init = simulate_gaussian_mixture(X, J);
 
 
@@ -188,52 +186,9 @@ for aa = 1:numel(n_vec)
     prop_init, gamma_init, B_cell_init, A_cell_init, ...
     1, 50, t_spike, temp, tau)
 
-        %% ----------------------------------------------------
-        % 8) Store results
-        % -----------------------------------------------------
-        tmp = struct();
-
-        % truth
-        tmp.N = Nsim;
-        tmp.X = X;
-        tmp.Z_true = Z_true;
-        tmp.A_true = A_true;
-        tmp.prop_true = prop_true;
-        tmp.gamma_true = gamma_true;
-        tmp.B_cell_true = B_cell_true;
-        tmp.B1_true_scale = B1_true_scale;
-        tmp.gamma_true_scale = gamma_true_scale;
-
-        % rank-likelihood objects
-        tmp.R = R;
-        tmp.Rlevels = Rlevels;
-        tmp.Ranks = Ranks;
-
-        % initial values
-        tmp.Z_init = Z_init;
-        tmp.prop_init = prop_init;
-        tmp.gamma_init = gamma_init;
-        tmp.B_cell_init = B_cell_init;
-        tmp.A_cell_init = A_cell_init;
-
-        % fit
-        tmp.fit = fit_out;
-
-        % supports
-        tmp.G1 = G1;
-        tmp.G2 = G2;
-        tmp.G3 = G3;
-
-        RESULTS{aa, c} = tmp;
+        RESULTS{aa, c} = fit_out;
     end
 
     fprintf('Finished sample size %d\n', Nsim);
 end
 
-%% ------------------------------------------------------------
-% Save
-% -------------------------------------------------------------
-save('RESULTS_Dlayer_DDE_depCUSP.mat', 'RESULTS', 'n_vec', ...
-     'C_sims', 'model', 'D', 'K1_true', 'K2_true', 'K3_true', ...
-     'J', 'prop_true', 'gamma_true', 'B_cell_true', ...
-     '-v7.3');
